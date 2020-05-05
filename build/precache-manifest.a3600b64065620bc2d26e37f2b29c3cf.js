57b850d5016c20d4bc671e2bab9406a166e196c1
Vu Minh Le <vuminhle@outlook.com>
2020-05-05 21:06:02 +0200

    new

---
+++ build/precache-manifest.a3600b64065620bc2d26e37f2b29c3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d44f740b67df85dbb756ac7c84096eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db0d808a5585b3d5d26",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b228ce08e20d8b19efe8",
+    "url": "/static/js/2.40fe2763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40fe2763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db0d808a5585b3d5d26",
+    "url": "/static/js/main.c351e534.chunk.js"
+  },
+  {
+    "revision": "322a1eb4e229f5cca269",
+    "url": "/static/js/runtime-main.2f104cb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
